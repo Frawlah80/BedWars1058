06bd94c36b7305df72d2d5fed718f29c4c1a01b6
Frawlah <88870560+Frawlah80@users.noreply.github.com>
2024-04-10 14:47:10 +0530

    Delete versionsupport_v1_19_R2 directory

--- versionsupport_v1_19_R2/src/main/java/com/andrei1058/bedwars/support/version/v1_19_R2/despawnable/DespawnableAttributes.java
+++
@@ -1,6 +0,0 @@
-package com.andrei1058.bedwars.support.version.v1_19_R2.despawnable;
-
-@Deprecated
-public record DespawnableAttributes(DespawnableType type, double speed, double health, double damage, int despawnSeconds) {
-
-}
--- versionsupport_v1_19_R2/src/main/java/com/andrei1058/bedwars/support/version/v1_19_R2/despawnable/DespawnableType.java
+++
@@ -1,7 +0,0 @@
-package com.andrei1058.bedwars.support.version.v1_19_R2.despawnable;
-
-@Deprecated
-public enum DespawnableType {
-    IRON_GOLEM,
-    SILVERFISH
-}
