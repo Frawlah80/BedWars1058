6a015b87cf753624ae458e4277b35b239f26d7f9
Frawlah <88870560+Frawlah80@users.noreply.github.com>
2024-04-10 14:43:33 +0530

    Delete bedwars-api directory

--- bedwars-api/src/main/java/com/andrei1058/bedwars/api/arena/stats/Incrementable.java
+++
@@ -1,6 +0,0 @@
-package com.andrei1058.bedwars.api.arena.stats;
-
-public interface Incrementable {
-
-    void increment();
-}
--- bedwars-api/src/main/java/com/andrei1058/bedwars/api/configuration/GameMainOverridable.java
+++
@@ -1,8 +0,0 @@
-package com.andrei1058.bedwars.api.configuration;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface GameMainOverridable {
-}
