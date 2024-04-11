16b120368c80442c17290d762743f192da06e8ac
Frawlah <frawlahmail@gmail.com>
2024-04-11 18:43:45 +0530

    ok

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/commands/mapcmd/MapCommand.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/commands/mapcmd/MapCommand.java
@@ -4,12 +4,10 @@ import com.andrei1058.bedwars.api.arena.GameState;
 import com.andrei1058.bedwars.api.arena.IArena;
 import com.andrei1058.bedwars.api.language.Messages;
 import com.andrei1058.bedwars.arena.Arena;
-import com.andrei1058.bedwars.support.papi.SupportPAPI;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 
 import static com.andrei1058.bedwars.api.language.Language.getMsg;
 
@@ -25,20 +23,15 @@ public class MapCommand extends BukkitCommand {
             return true;
         }
         Player p = (Player) sender;
-        replaceArenaPlaceholders(p, getMsg(p, Messages.COMMAND_MAP_ARENA_NAME), true);
         IArena arena = Arena.getArenaByPlayer(p);
         if (args.length == 0) {
             if (arena == null) {
                 p.sendMessage(getMsg(p, Messages.COMMAND_MAP_NOT_IN_ARENA));
                 return true;
-            } else if (arena.getStatus() == GameState.playing) {
-                p.sendMessage(getMsg(p, Messages.COMMAND_MAP_ARENA_NAME));
-                return true;
-            } else if (arena.getStatus() == GameState.waiting) {
-                p.sendMessage(getMsg(p, Messages.COMMAND_MAP_ARENA_NAME));
-                return true;
-            } else if (arena.getStatus() == GameState.starting) {
-                p.sendMessage(getMsg(p, Messages.COMMAND_MAP_ARENA_NAME));
+            } else if (arena.getStatus() == GameState.playing || arena.getStatus() == GameState.waiting || arena.getStatus() == GameState.starting) {
+                p.sendMessage(getMsg(p, Messages.COMMAND_MAP_ARENA_NAME)
+                        .replace("{arenaDisplayName}", arena.getDisplayName())
+                        .replace("{arenaName}", arena.getArenaName()));
                 return true;
             } else {
                 return false;
@@ -46,14 +39,4 @@ public class MapCommand extends BukkitCommand {
         } return true;
     }
 
-    public static void replaceArenaPlaceholders(Player player, @NotNull String s, boolean papiReplacements) {
-        IArena a = Arena.getArenaByPlayer(player);
-
-        if (s.contains("{arenaDisplayName}")) s = s.replace("{arenaDisplayName}", a.getDisplayName());
-        if (s.contains("{arenaName}")) s = s.replace("{arenaName}", a.getArenaName());
-
-        if (papiReplacements) {
-            SupportPAPI.getSupportPAPI().replace(player, s);
-        }
-    }
 }
