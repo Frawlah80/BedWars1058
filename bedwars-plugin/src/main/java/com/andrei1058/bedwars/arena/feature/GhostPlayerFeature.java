016e053e62ac44a77afe35294f671312191bdf10
Frawlah80 <frawlahmail@gmail.com>
2024-05-14 14:14:01 +0530

    ghost todo

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/feature/GhostPlayerFeature.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/arena/feature/GhostPlayerFeature.java
@@ -1,34 +1,30 @@
 package com.andrei1058.bedwars.arena.feature;
 
+import com.andrei1058.bedwars.BedWars;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.scoreboard.Team;
 
-@SuppressWarnings("deprecation")
 public class GhostPlayerFeature {
 
-    private static ScoreboardManager sbMgr;
-    private static Scoreboard board;
-    private static Team sbTeam;
-
     public static void addPlayerAsGhost(Player p) {
-        sbMgr = Bukkit.getScoreboardManager();
-        board = sbMgr.getMainScoreboard();
-        sbTeam = board.getTeam("gteam");
-        if (sbTeam == null) {
-            sbTeam = board.registerNewTeam("gteam");
-        }
-        sbTeam.setAllowFriendlyFire(false);
-        sbTeam.setCanSeeFriendlyInvisibles(true);
-        p.setScoreboard(board);
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Team ghosts = board.registerNewTeam("Ghost");
+        ghosts.setDisplayName("Spectating Ghosts");
+        ghosts.setCanSeeFriendlyInvisibles(true);
+        ghosts.setAllowFriendlyFire(false);
+        ghosts.addEntry(p.getName());
+        BedWars.debug("Added " + p.getDisplayName() + " is now added to team " + ghosts.getDisplayName());
     }
 
     public static void removePlayerAsGhost(Player p) {
-        sbMgr = Bukkit.getScoreboardManager();
-        board = sbMgr.getMainScoreboard();
-        sbTeam = board.getTeam("gteam");
-        sbTeam.removePlayer(p);
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+        Team ghosts = board.getTeam("Ghost");
+        ghosts.removeEntry(p.getName());
     }
 }
